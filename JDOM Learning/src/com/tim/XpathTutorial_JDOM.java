81d7f838be48ad0ca9204ed84ca3cd9c957e6598
tteller <tim.teller@gmail.com>
2015-03-11 17:21:29 -0600

    Update XpathTutorial_JDOM.java

--- JDOM Learning/src/com/tim/XpathTutorial_JDOM.java
+++ JDOM Learning/src/com/tim/XpathTutorial_JDOM.java
@@ -4,102 +4,160 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
+import org.jdom2.xpath.XPath;
 import org.jdom2.xpath.jaxen.JDOMXPath;
 
 @SuppressWarnings("deprecation")
 public class XpathTutorial_JDOM {
 
-public static void main(String[] args) throws IOException {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException {
 
-// Open a XML File
-SAXBuilder sax = new SAXBuilder();
-Document doc = null;
-try {
-doc = sax.build("files/getCopyEventNotes.xml");
-} catch (JDOMException e2) {
-// TODO Auto-generated catch block
-e2.printStackTrace();
-} catch (IOException e2) {
-// TODO Auto-generated catch block
-e2.printStackTrace();
-}
-// System.out.println(doc);
+		// Open a XML File
+		SAXBuilder sax = new SAXBuilder();
+		Document doc = null;
+		try {
+			doc = sax.build("files/isGroupStaffPlanRequest.xml");
+		} catch (JDOMException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 
-// XPath Stuff
-List<String> xPathInstance = Arrays.asList( "//*[local-name()='fromDBInstance']", "//*[@instance]",
-"//*[local-name()='DBInstance'");
-// System.out.println(xPathInstance);
-JDOMXPath xPathExpression = null;
-Element getElement = null;
-boolean instanceExists = false;
-for (int x = 0; x < xPathInstance.size(); x++) {
+		// XPath Stuff
+		List<String> xPathInstance = Arrays.asList(
+
+		"//*[local-name()='fromDBInstance' or local-name()='DBInstance']",
+				"//*[local-name()='@instance']");
+
+		JDOMXPath xPathExpression = null;
+		Element getElement = null;
+		boolean instanceExists = false;
+		boolean done = false;
+		for (int x = 0; x < xPathInstance.size(); x++) {
+			getElement = null;
+
+			/*
+			 * try { xPathExpression = new JDOMXPath(xPathInstance.get(x)); //
+			 * System.out.println(xPathExpression); } catch (JDOMException e1) {
+			 * // TODO Auto-generated catch block e1.printStackTrace(); }
+			 */
+			try {
+
+				getElement = (Element) JDOMXPath.selectSingleNode(doc,
+						xPathInstance.get(x));
+
+				//TODO:  Figure out this next line.
 
 
-try {
-xPathExpression = new JDOMXPath(xPathInstance.get(x));
-System.out.println(xPathExpression);
-} catch (JDOMException e1) {
-// TODO Auto-generated catch block
-e1.printStackTrace();
-}
-try {
+				System.out.println(getElement + " " + x);
+				System.out.println(doc);
 
-getElement = (Element) xPathExpression.selectSingleNode(doc);
-} catch (JDOMException e1) {
-// TODO Auto-generated catch block
-	System.out.println("didn't work");
-e1.printStackTrace();
-}
-//System.out.println(getElement.toString());
-//String getTheElementText = getElement.toString();
-//System.out.println(getElement.getText());
-System.out.println(getElement);
+				
+				/*if ((((List<String>) getElement).isEmpty())) {
+					System.out.println("Tim Rocks");
+					}
+				*/	
+				
 
-while (instanceExists == false && getElement != null) {
-//System.out.println(getElement.getAttributeValue("instance"));
+			} catch (JDOMException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("didn't work");
+				e1.printStackTrace();
+			}
 
-	// TODO write a method to pass in the getElement object and return result
-	
-	
-	
-	
-if (getElement.getAttributeValue("instance").contains("401K")) {
-outPutXml(doc, "QASK", getElement);
-instanceExists = true;
-} else if (getElement.getAttributeValue("instance").contains(
-"PNP")) {
-outPutXml(doc, "QASP", getElement);
-instanceExists = true;
-} else if (getElement.getAttributeValue("instance").contains(
-"INST")) {
-outPutXml(doc, "QASI", getElement);
-instanceExists = true;
-} else if (getElement.getAttributeValue("instance").contains(
-"IN02")) {
-outPutXml(doc, "QAS2", getElement);
-instanceExists = true;
-} else
-System.out.println("nothing found");
+			while (!instanceExists && !done && getElement != null) {
+				// TODO might want to change this to a CASE thing
 
-}
+				switch (x) {
 
-}
+				case 0: {
 
-}
+					if (getElement.getText().contains("401K")) {
+						outPutXml(doc, "QASK", getElement, 0);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getText().contains("PNP")) {
+						outPutXml(doc, "QASP", getElement, 0);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getText().contains("INST")) {
+						outPutXml(doc, "QASI", getElement, 0);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getText().contains("IN02")) {
+						outPutXml(doc, "QAS2", getElement, 0);
+						instanceExists = true;
+						done = true;
+						break;
+					} else
+						System.out.println("nothing found");
+					break;
+				}
 
-private static void outPutXml(Document doc, String setInstance,
-Element getElement) throws IOException {
-// Outputting - Changing the XML
+				case 1: {
 
-System.out.println(setInstance);
-getElement.setAttribute("instance", setInstance);
-XMLOutputter xmlOutput = new XMLOutputter();
-xmlOutput.output(doc, System.out);
+					if (getElement.getAttributeValue("instance").contains(
+							"401K")) {
+						outPutXml(doc, "QASK", getElement, 1);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getAttributeValue("instance")
+							.contains("PNP")) {
+						outPutXml(doc, "QASP", getElement, 1);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getAttributeValue("instance")
+							.contains("INST")) {
+						outPutXml(doc, "QASI", getElement, 1);
+						instanceExists = true;
+						done = true;
+						break;
+					} else if (getElement.getAttributeValue("instance")
+							.contains("IN02")) {
+						outPutXml(doc, "QAS2", getElement, 1);
+						instanceExists = true;
+						done = true;
+						break;
+					} else
+						System.out.println("nothing found");
+					break;
+				}
 
-}
+				}
+			}
+
+		}
+
+	}
+
+	private static void outPutXml(Document doc, String setInstance,
+			Element getElement, int t) throws IOException {
+		// Outputting - Changing the XML
+		if (t == 1) {
+			System.out.println(setInstance);
+			getElement.setAttribute("instance", setInstance);
+			XMLOutputter xmlOutput = new XMLOutputter();
+			xmlOutput.output(doc, System.out);
+		} else {
+			System.out.println(setInstance);
+			getElement.setText(setInstance);
+			XMLOutputter xmlOutput = new XMLOutputter();
+			xmlOutput.output(doc, System.out);
+		}
+
+	}
 }
