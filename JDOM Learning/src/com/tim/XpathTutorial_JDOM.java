fd1591b57dfa966333980a26163954e7526099fa
Tim Teller <tim.teller@gmail.com>
2015-03-16 07:19:56 -0600

    Finished with the new XPathFactory methods

--- JDOM Learning/src/com/tim/XpathTutorial_JDOM.java
+++ JDOM Learning/src/com/tim/XpathTutorial_JDOM.java
@@ -37,16 +37,12 @@ public class XpathTutorial_JDOM {
 	public static void main(String[] args) throws IOException {
 
 		getProperties();
-/*		System.out.println(xmlFile + " " + xpathQry + " " + fromDBInstance
-				+ " " + dbInstance + " " + db401K + " " + dbINST + " " + dbPNP
-				+ " " + dbIN02);
-*/
+
 		// Open a XML File
 		SAXBuilder sax = new SAXBuilder();
 		Document doc = null;
 		try {
-			doc = sax
-					.build(xmlFile);
+			doc = sax.build(xmlFile);
 		} catch (JDOMException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
@@ -56,12 +52,6 @@ public class XpathTutorial_JDOM {
 		}
 
 		// XPath Stuff
-		/*
-		 * List<String> xPathInstance = Arrays.asList(
-		 * 
-		 * "//*[local-name()='fromDBInstance' or local-name()='DBInstance']",
-		 * "//*[@instance]");
-		 */
 
 		XPathFactory xpFactory = XPathFactory.instance();
 
@@ -74,71 +64,46 @@ public class XpathTutorial_JDOM {
 			Element elm = (Element) xPathSrchNodes.get(i);
 			System.out.println("Content val: " + content.getValue());
 			System.out.println("Element Val: " + elm.getValue());
-			
-			//TODO: need to do the conditions in here.  If the element is null, then the value is in the 
-			elm.setAttribute("instance", "QASP");
-
-			elm.setText("QASK");
 
 			System.out.println("Element Val after changing: " + elm.getValue());
-			
-			System.out.println("AttributeName: "
-					+ elm.getAttributeValue("instance"));
+
+			System.out.println("AttributeName: " + attrInstance.toLowerCase()
+					+ " " + elm.getAttributeValue(attrInstance));
+			String attr = elm.getAttributeValue(attrInstance);
+			if (attr == null) {
+				System.out.println("You aren't using an attribute ");
+				if (elm.getText().contains("401K")) {
+					elm.setText(db401K);
+				} else if (elm.getText().contains("PNP")) {
+					elm.setText(dbPNP);
+				} else if (elm.getText().contains("INST")) {
+					elm.setText(dbINST);
+				} else if (elm.getText().contains("IN02")) {
+					elm.setText(dbIN02);
+				} else {
+					System.out.println("nothing found");
+				}
+			} else {
+				System.out.println("You ARE using an attribute ");
+				if (elm.getAttributeValue(attrInstance).contains("401K")) {
+					elm.setAttribute(attrInstance, db401K);
+				} else if (elm.getAttributeValue(attrInstance).contains("PNP")) {
+					elm.setAttribute(attrInstance, dbPNP);
+				} else if (elm.getAttributeValue(attrInstance).contains("INST")) {
+					elm.setAttribute(attrInstance, dbINST);
+				} else if (elm.getAttributeValue(attrInstance).contains("IN02")) {
+					elm.setAttribute(attrInstance, dbIN02);
+				} else
+					System.out.println("nothing found");
+			}
+
+			// System.out.println(elm.getContentSize());
 
 			XMLOutputter xmlOutput = new XMLOutputter();
 			xmlOutput.output(doc, System.out);
 
 		}
 
-		/*
-		 * 
-		 * 
-		 * Element getElement = null; boolean instanceExists = false; boolean
-		 * done = false; for (int x = 0; x < xPathInstance.size(); x++) {
-		 * getElement = null;
-		 * 
-		 * try {
-		 * 
-		 * getElement = (Element) JDOMXPath.selectSingleNode(doc,
-		 * xPathInstance.get(x));
-		 * 
-		 * } catch (JDOMException e1) { // TODO Auto-generated catch block
-		 * System.out.println("didn't work"); e1.printStackTrace(); }
-		 * 
-		 * while (!instanceExists && !done && getElement != null) {
-		 * 
-		 * switch (x) {
-		 * 
-		 * case 0: {
-		 * 
-		 * if (getElement.getText().contains("401K")) { outPutXml(doc, "QASK",
-		 * getElement, 0); instanceExists = true; done = true; break; } else if
-		 * (getElement.getText().contains("PNP")) { outPutXml(doc, "QASP",
-		 * getElement, 0); instanceExists = true; done = true; break; } else if
-		 * (getElement.getText().contains("INST")) { outPutXml(doc, "QASI",
-		 * getElement, 0); instanceExists = true; done = true; break; } else if
-		 * (getElement.getText().contains("IN02")) { outPutXml(doc, "QAS2",
-		 * getElement, 0); instanceExists = true; done = true; break; } else
-		 * System.out.println("nothing found"); break; }
-		 * 
-		 * case 1: {
-		 * 
-		 * if (getElement.getAttributeValue("instance").contains( "401K")) {
-		 * outPutXml(doc, "QASK", getElement, 1); instanceExists = true; done =
-		 * true; break; } else if (getElement.getAttributeValue("instance")
-		 * .contains("PNP")) { outPutXml(doc, "QASP", getElement, 1);
-		 * instanceExists = true; done = true; break; } else if
-		 * (getElement.getAttributeValue("instance") .contains("INST")) {
-		 * outPutXml(doc, "QASI", getElement, 1); instanceExists = true; done =
-		 * true; break; } else if (getElement.getAttributeValue("instance")
-		 * .contains("IN02")) { outPutXml(doc, "QAS2", getElement, 1);
-		 * instanceExists = true; done = true; break; } else
-		 * System.out.println("nothing found"); break; }
-		 * 
-		 * } }
-		 * 
-		 * }
-		 */
 	}
 
 	private static void getProperties() throws FileNotFoundException,
@@ -147,7 +112,7 @@ public class XpathTutorial_JDOM {
 		props = new Properties();
 		props.load(is);
 
-		System.out.println("There are: " + props.size() + " properties");
+		// System.out.println("There are: " + props.size() + " properties");
 
 		xmlFile = props.getProperty("xmlFile");
 		xpathQry = props.getProperty("xpathQry");
@@ -159,23 +124,6 @@ public class XpathTutorial_JDOM {
 		dbPNP = props.getProperty("dbPNP");
 		dbIN02 = props.getProperty("dbIN02");
 
-
 	}
 
-	private static void outPutXml(Document doc, String setInstance,
-			Element getElement, int t) throws IOException {
-		// Outputting - Changing the XML
-		if (t == 1) {
-			System.out.println(setInstance);
-			getElement.setAttribute("instance", setInstance);
-			XMLOutputter xmlOutput = new XMLOutputter();
-			xmlOutput.output(doc, System.out);
-		} else {
-			System.out.println(setInstance);
-			getElement.setText(setInstance);
-			XMLOutputter xmlOutput = new XMLOutputter();
-			xmlOutput.output(doc, System.out);
-		}
-
-	}
 }
